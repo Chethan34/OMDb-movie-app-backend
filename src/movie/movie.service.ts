c027e5fa05bc92eccd297652fed248aabbf9005b
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2024-03-10 17:04:33 +0900

    feat: removed unused endpoints

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
+// import { UpdateMovieDto } from './dto/update-movie.dto';
 import { MovieDto } from './dto/movie.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Movie } from './entities/movie.entity';
@@ -34,23 +34,23 @@ export class MovieService {
     return this.movieRepository.findOneOrFail({ where: { imdb_id } });
   }
 
-  async update(id: number, updateMovieDto: UpdateMovieDto): Promise<MovieDto> {
-    const movie = await this.movieRepository.findOneOrFail({ where: { id } });
+  // async update(id: number, updateMovieDto: UpdateMovieDto): Promise<MovieDto> {
+  //   const movie = await this.movieRepository.findOneOrFail({ where: { id } });
 
-    if (updateMovieDto.imdb_id) {
-      movie.imdb_id = updateMovieDto.imdb_id;
-    }
-    if (updateMovieDto.title) {
-      movie.title = updateMovieDto.title;
-    }
+  //   if (updateMovieDto.imdb_id) {
+  //     movie.imdb_id = updateMovieDto.imdb_id;
+  //   }
+  //   if (updateMovieDto.title) {
+  //     movie.title = updateMovieDto.title;
+  //   }
 
-    return this.movieRepository.save(movie);
-  }
+  //   return this.movieRepository.save(movie);
+  // }
 
-  async remove(id: number): Promise<MovieDto> {
-    const movie = await this.movieRepository.findOneOrFail({ where: { id } });
-    return this.movieRepository.remove(movie);
-  }
+  // async remove(id: number): Promise<MovieDto> {
+  //   const movie = await this.movieRepository.findOneOrFail({ where: { id } });
+  //   return this.movieRepository.remove(movie);
+  // }
 
   apiTest(): string {
     return this.apiService.getHello();
